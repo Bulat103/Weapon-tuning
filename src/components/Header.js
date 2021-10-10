24c939772fdb15fad35a09da99cca71efa364bfb
Bulat Khisamov <hisbul@mail.ru>
2021-10-10 17:47:13 +0300

    Add NavBar, Add infinity slider in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,67 @@
+import { useEffect } from "react";
 import { NavLink } from "react-router-dom"
 function Header() {
+
+    useEffect(() => {
+        let slides = document.querySelectorAll('.slide-single');
+        console.log(slides);
+        let slider = [];
+        for (let i = 0; i < slides.length; i++) {
+            slider[i] = slides[i].src;
+            slides[i].remove();
+        }
+        console.log(slider);
+        let step = 0;
+        let offset = 0;
+
+        function draw() {
+            let img = document.createElement('img');
+            img.src = slider[step]
+            img.classList.add('slide-single')
+            img.style.left = offset * 2000 + 'px';
+            document.querySelector('#slide').appendChild(img);
+            if (step + 1 == slider.length) {
+                step = 0;
+            }
+            else {
+                step++
+            }
+            offset = 1;
+        }
+        function left() {
+            let slides2 = document.querySelectorAll('.slide-single');
+            let offset2 = 0;
+            for (let i = 0; i < slides2.length; i++) {
+                slides2[i].style.left = offset2 * 2000 - 2000 + "px"
+                offset2++;
+            }
+            setTimeout(() => {
+                slides2[0].remove();
+                draw();
+            }, 9500)
+
+        }
+        setInterval(() => {
+            left();
+        }, 10000)
+
+
+        draw(); draw();
+    });
+
+
+
     return (
         <div className="header-container">
+            <div id="slide">
+                <img src="/img/1.jpg" className="slide-single" alt="" />
+                <img src="/img/2.jpg" className="slide-single" alt="" />
+                <img src="/img/3.jpg" className="slide-single" alt="" />
+                <img src="/img/4.jpg" className="slide-single" alt="" />
+            </div>
+
             <div className="nav-container">
+
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="#">ShareNote</a>
@@ -16,7 +75,6 @@ function Header() {
                                 <li className="nav-item "><a className="nav-link " href="#services">Services</a></li>
                                 <li className="nav-item "><a className="nav-link " href="#about">About</a></li>
                                 <li className="nav-item "> <a className="nav-link " href="#footer">Contacts</a></li>
-
                             </ul>
                         </div>
                     </div>
